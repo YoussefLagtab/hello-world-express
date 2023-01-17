5d576aed15bf2a411034d9c0b90f1621b11f7a0f
Youssef Lagtab <youssef.lagtab@spotter.com>
2023-01-17 19:36:43 +0100

    create 4 server running on 4 ports

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-const app = require('express')();
+const express = require('express');
 
-app.get('/', (_req, res) => {
-  res.send('Hello from VM1\n');
+const createRouteMiddleware = port => (_req, res) => {
+  res.send(`Hello from server port: ${port}\n`);
+};
+
+[3000,3001,3002,3003].map(port => {
+  const app = express();
+
+  app.get('/', createRouteMiddleware(port));
+
+  app.listen(port, () => console.log("Server running on port", port));
 });
-
-const port = 3000;
-app.listen(port, () => console.log('Server running on port', port));
